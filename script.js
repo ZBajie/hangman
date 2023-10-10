647384f88d22a5b9c905f117f81b2209b73ebd4c
Lars Abelsson <georg483@msn.com>
2023-10-10 17:44:39 +0200

    buuton fix

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-
-const number = 1;
-switch(number){
-    case 1: 
-    hangmanGame();
-
-    
+const number = 1
+switch (number) {
+  case 1:
+    hangmanGame()
 }
 
-function hangmanGame(){
-    const listOfWords = ['january', 'monday','elephant', 'summer', 'lawyer','school']
-    word = listOfWords[ Math.floor(Math.random() * listOfWords.length)];
-    console.log(word)
-    linesForLetterInWord(word)
-    document.querySelector('#forGuessbutton').addEventListener('Click', userLetter);
+function hangmanGame() {
+  const listOfWords = [
+    "january",
+    "monday",
+    "elephant",
+    "summer",
+    "lawyer",
+    "school",
+  ]
+  word = listOfWords[Math.floor(Math.random() * listOfWords.length)]
+  console.log(word)
+  linesForLetterInWord(word)
+  const userGuess = document.querySelector("#forguessbutton")
+  userGuess.addEventListener("click", userLetter)
 }
 
-function linesForLetterInWord(word){
-    const letterList = document.querySelector('#letterlist')
-    for (let i = 0; i < word.length; i++){
-        const letterItem = document.createElement('li')
-        letterItem.innerText = '_';
-        letterList.append(letterItem)
-        
-    }
-
+function linesForLetterInWord(word) {
+  const letterList = document.querySelector("#letterlist")
+  for (let i = 0; i < word.length; i++) {
+    const letterItem = document.createElement("li")
+    letterItem.innerText = "_"
+    letterList.append(letterItem)
+  }
 }
 
-function userLetter(){
-
-    let letter = document.querySelector('#forGuess').value
-    console.log(letter);
+function userLetter() {
+  let letter = document.querySelector("#forguess").value
+  console.log("letter", letter)
 }
-userLetter();
