40d56c6bff7cb63117d73f4b0e413a0f08e93ad8
Lars Abelsson <georg483@msn.com>
2023-12-29 19:30:11 +0100

    Changed to swedish and import json file with words

--- script.js
+++ script.js
@@ -1,127 +1,133 @@
-let word = "";
+let word = ""
 
-const guessedLetters = [];
+const guessedLetters = []
 
-function hangmanGame() {
-  const listOfWords = [
+async function hangmanGame() {
+  let listOfWords = [
     "january",
     "monday",
     "elephant",
     "summer",
     "lawyer",
     "school",
-  ];
-  word = listOfWords[Math.floor(Math.random() * listOfWords.length)];
-  linesForLetterInWord(word);
-  const userGuess = document.querySelector("#forguessbutton");
-  userGuess.addEventListener("click", userLetter);
+  ]
+
+  await fetch("cities_of_sweden.json")
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res)
+      listOfWords = res
+    })
+  console.log(listOfWords)
+  word = listOfWords[Math.floor(Math.random() * listOfWords.length)]
+  linesForLetterInWord(word)
+  const userGuess = document.querySelector("#forguessbutton")
+  userGuess.addEventListener("click", userLetter)
 }
 
 function linesForLetterInWord(word) {
-  const letterList = document.querySelector("#letterlist");
+  const letterList = document.querySelector("#letterlist")
   for (let i = 0; i < word.length; i++) {
-    const letterItem = document.createElement("li");
-    letterItem.innerText = "_";
-    letterList.append(letterItem);
+    const letterItem = document.createElement("li")
+    letterItem.innerText = "_"
+    letterList.append(letterItem)
   }
 }
 
-hangmanGame();
+hangmanGame()
 
 function userLetter() {
-  let letter = document.querySelector("#forguess").value;
+  let letter = document.querySelector("#forguess").value
   if (
     event.type === "click" ||
     (event.type === "keydown" && event.key === "Enter")
   ) {
     if (
       letter.length === 1 &&
-      letter.match(/[a-z]/i) &&
+      letter.match(/[a-zåäö]/i) &&
       guessedLetters.indexOf(letter) === -1
     ) {
-      guessedLetters.push(letter);
-      testGuessLetter(letter);
+      guessedLetters.push(letter)
+      testGuessLetter(letter)
     } else if (guessedLetters.indexOf(letter) !== -1) {
-      const wordContainer = document.querySelector(".wordcontainer");
-      const pElement = document.createElement("p");
-      pElement.innerText = "You've already guessed this letter!";
-      pElement.classList.add("sameletter");
-      wordContainer.appendChild(pElement);
+      const wordContainer = document.querySelector(".wordcontainer")
+      const pElement = document.createElement("p")
+      pElement.innerText = "Du har redan gissat på denna bokstaven!"
+      pElement.classList.add("sameletter")
+      wordContainer.appendChild(pElement)
 
       setTimeout(function () {
-        wordContainer.removeChild(pElement);
-      }, 3000);
+        wordContainer.removeChild(pElement)
+      }, 3000)
     } else {
-      alert("Wrong input! Enter only (1) letter without special characters.");
+      alert("Fel inmatning! Skriv bara en bokstav.")
     }
-    document.querySelector("#forguess").value = "";
-    document.querySelector("#forguess").focus();
+    document.querySelector("#forguess").value = ""
+    document.querySelector("#forguess").focus()
   }
 }
 
-document.querySelector("#forguessbutton").addEventListener("click", userLetter);
-document.querySelector("#forguess").addEventListener("keydown", userLetter);
+document.querySelector("#forguessbutton").addEventListener("click", userLetter)
+document.querySelector("#forguess").addEventListener("keydown", userLetter)
 
-let wrongGuesses = 0;
+let wrongGuesses = 0
 
 function handleWrongGuess() {
-  wrongGuesses++;
+  wrongGuesses++
   if (wrongGuesses === 1) {
-    document.getElementById("head").style.display = "block";
+    document.getElementById("head").style.display = "block"
   } else if (wrongGuesses === 2) {
-    document.getElementById("body").style.display = "block";
+    document.getElementById("body").style.display = "block"
   } else if (wrongGuesses === 3) {
-    document.getElementById("arms").style.display = "block";
+    document.getElementById("arms").style.display = "block"
   } else if (wrongGuesses === 4) {
-    document.getElementById("legs").style.display = "block";
+    document.getElementById("legs").style.display = "block"
   } else {
-    alert("Something went wrong, restart the game and try again!");
+    alert("Something went wrong, restart the game and try again!")
   }
 }
 
-const maxWrongGuesses = 4;
-const guessWordArray = new Array(word.length);
+const maxWrongGuesses = 4
+const guessWordArray = new Array(word.length)
 
 function testGuessLetter(letter) {
-  let found = false;
+  let found = false
   for (let i = 0; i < word.length; i++) {
     if (letter.toLowerCase() === word[i]) {
-      let k = i + 1; // Use k = i + 1 because nth:child start at 1 and cant do i + 1 inside queryselctor.
+      let k = i + 1 // Use k = i + 1 because nth:child start at 1 and cant do i + 1 inside queryselctor.
       let placeNewLetter = document.querySelector(
         "#letterlist li:nth-child(" + k + ")"
-      );
-      placeNewLetter.innerText = letter.toLowerCase();
-      guessWordArray[i] = letter.toLowerCase();
-      found = true;
+      )
+      placeNewLetter.innerText = letter.toLowerCase()
+      guessWordArray[i] = letter.toLowerCase()
+      found = true
     }
-    let guessWordArrayString = guessWordArray.join("");
+    let guessWordArrayString = guessWordArray.join("")
     if (word === guessWordArrayString) {
       setTimeout(function () {
-        alert(`You won, play again? 😍 `);
-        newGame();
-      }, 500);
+        alert(`Du vann, spela igen? 😍 `)
+        newGame()
+      }, 500)
     }
   }
   if (!found) {
-    handleWrongGuess();
+    handleWrongGuess()
     if (wrongGuesses === maxWrongGuesses) {
-      document.getElementById("legs");
+      document.getElementById("legs")
       setTimeout(function () {
-        alert(
-          `You lost unfortunately 😕, the corect word was: ${word}! Try again? 😍 `
-        );
-        newGame();
-      }, 500);
+        alert(`Du förlorade 😕, rätt ord var: ${word}! spela igen? 😍 `)
+        newGame()
+      }, 500)
     }
   }
 }
 
 function newGame() {
-  wrongGuesses = 0;
-  document.getElementById("head").style.display = "none";
-  document.getElementById("body").style.display = "none";
-  document.getElementById("arms").style.display = "none";
-  document.getElementById("legs").style.display = "none";
+  wrongGuesses = 0
+  document.getElementById("head").style.display = "none"
+  document.getElementById("body").style.display = "none"
+  document.getElementById("arms").style.display = "none"
+  document.getElementById("legs").style.display = "none"
   linesForLetterInWord(wrongGuesses)
-  window.location.reload();
+  window.location.reload()
 }
